b925dbc33ec46e3601d474459d07951113712c37
Carlos Javier <carloshervas63@hotmail.com>
2022-11-28 18:37:33 +0100

    Simple Diagram View done (now needs Controller)

--- tfg/src/main/java/com/example/DiagramPresenter.java
+++ tfg/src/main/java/com/example/DiagramPresenter.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class DiagramPresenter {
     private final Diagram model;
     private final DiagramView view;
--- tfg/src/main/java/com/example/DiagramView.java
+++ tfg/src/main/java/com/example/DiagramView.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -12,19 +13,33 @@ public class DiagramView {
         }
 
         private Diagram model;
+        @FXML
         TextField parameters = new TextField();
+        @FXML
         Label originalDataSolutionArrow = new Label("=====================>");
+        @FXML
         Label originalSolution = new Label();
+        @FXML
         Label datasArrow = new Label(" |\n |\n |\n |\n |");
+        @FXML
         ComboBox reductionSelect = new ComboBox();
+        @FXML
         Label subParameters = new Label();
+        @FXML
         Label partialSolution = new Label();
+        @FXML
         ComboBox solutionSelect = new ComboBox();
+        @FXML
         VBox NandBaseCaseBox = new VBox();
+        @FXML
         ComboBox problemSizeSelect = new ComboBox();
+        @FXML
         ComboBox baseCaseSelect = new ComboBox();
+        @FXML
         Label partialDataSolutionArrow = new Label("=====================>");
+        @FXML
         Label solutionsArrow = new Label(" |\n |\n |\n |\n |");
+        @FXML
         Button confirmDataButton = new Button("Confirmar datos");
 
         public DiagramView(){
@@ -33,49 +48,9 @@ public class DiagramView {
         public DiagramView(Diagram model) {
                 this.model = model;
                 this.root=new GridPane();
-                setLayout();
-                setStyle();
                 bindModelData();
         }
 
-        private void setLayout() {
-                root.setHgap(10);
-                root.setVgap(10);
-                root.setPadding(new Insets(10, 10, 10, 10));
-
-                NandBaseCaseBox.setPadding(new Insets(5,5,5,5));
-                NandBaseCaseBox.getChildren().addAll(problemSizeSelect,baseCaseSelect);
-
-                root.add(NandBaseCaseBox,0,0);
-                root.add(parameters,1,0);
-                root.add(originalDataSolutionArrow,2,0);
-                root.add(originalSolution,3,0);
-                root.add(datasArrow,1,1);
-                root.add(reductionSelect,0,1);
-                root.add(confirmDataButton,0,2);
-                root.add(subParameters,1,3);
-                root.add(partialDataSolutionArrow,2,3);
-                root.add(partialSolution,3,3);
-                root.add(solutionsArrow,3,1);
-                root.add(solutionSelect,3,2);
-
-        }
-        private void setStyle() {
-                root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
-                        + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
-                        + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-
-                parameters.setStyle("-fx-font: 24 arial;");
-                reductionSelect.setStyle("-fx-font: 24 arial;");
-                solutionSelect.setStyle("-fx-font: 24 arial;");
-                originalDataSolutionArrow.setStyle("-fx-font: 24 arial;");
-                originalSolution.setStyle("-fx-font: 24 arial;");
-                datasArrow.setStyle("-fx-font: 24 arial;");
-                subParameters.setStyle("-fx-font: 24 arial;");
-                partialDataSolutionArrow.setStyle("-fx-font: 24 arial;");
-                partialSolution.setStyle("-fx-font: 24 arial;");
-                solutionsArrow.setStyle("-fx-font: 24 arial;");
-        }
         private void bindModelData() {
                 parameters.textProperty().bindBidirectional(model.getInputsProperty());
                 originalSolution.textProperty().bind(model.originalSolProperty());
@@ -89,9 +64,5 @@ public class DiagramView {
                 inputErrorAlert.setContentText("Revisa el contenido, debe ser en el formato 'A,B' ");
                 inputErrorAlert.showAndWait();
         }
-        public GridPane getRoot(){
-                return this.root;
-        }
-
         
 }
