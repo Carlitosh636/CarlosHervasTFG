6dbb2066560c1b2746fca3e1b2b57acdc7aa412c
Carlos Hervás <carlos.hervas@kairosds.com>
2023-07-02 23:16:00 +0200

    fix: fixed base cases for equal strings

--- tfg/src/main/java/com/example/diagrams/EqualStringsDiagram.java
+++ tfg/src/main/java/com/example/diagrams/EqualStringsDiagram.java
@@ -5,6 +5,7 @@ import com.example.algorithms.Algorithms;
 import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class EqualStringsDiagram implements IDiagramActions{
     Map<Integer, Callable<Map<String,String>>> algorithmMap= new HashMap<>();
@@ -38,10 +39,23 @@ public class EqualStringsDiagram implements IDiagramActions{
     }
 
     @Override
-    public Map<String, String> setGenCodeParams(String problemSize, String returnValue) {
+    public Map<String, String> setGenCodeParams(String baseCase, String returnValue) {
         Map<String,String> returnVal = new HashMap<>();
-        returnVal.put("baseCase",String.format("if %s == %s:",problemSize,""));
-        returnVal.put("returnValue",String.format("return %s",returnValue));
+        if(baseCase.contains("\n")){
+            StringBuilder stringBuilder = new StringBuilder();
+            List<String> baseCases = Stream.of(baseCase.split("\n")).map(s -> "\tif " + s + ":\n").toList();
+            List<String> returnValues = Stream.of(returnValue.split("\n")).map(s -> "\t\treturn " + s+"\n").toList();
+            for(int i = 0; i<baseCases.size();i++){
+                stringBuilder.append(baseCases.get(i));
+                stringBuilder.append(returnValues.get(i));
+            }
+            returnVal.put("baseCase",stringBuilder.toString());
+            returnVal.put("returnValue","");
+        }
+        else{
+            returnVal.put("baseCase",String.format("if %s:",baseCase));
+            returnVal.put("returnValue",String.format("return %s",returnValue));
+        }
         return returnVal;
     }
 
